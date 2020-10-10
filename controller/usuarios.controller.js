a874386f2db1e45842dcf630baa810a0080d02d0
Alvaro <enriquealvaro9@gmail.com>
2020-10-10 11:37:51 -0500

    fix docker

--- controller/usuarios.controller.js
+++ controller/usuarios.controller.js
@@ -3,7 +3,7 @@ const { response } = require('express');
 const getUsuarios = (req, res = response) => {
     res.json({
         ok: true,
-        msg: 'Datos solicitados correctamente'
+        msg: 'Datos solicitados correctamente!'
     });
 };
 
